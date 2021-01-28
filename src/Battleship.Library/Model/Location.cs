616d9bbd6d8856ded6dd9c9fbfb82a52765c1afb
Zsolt Keczer <keczerzsolt@gmail.com>
2021-01-29 00:29:32 +0100

    location class

--- src/Battleship.Library/Model/Location.cs
+++ src/Battleship.Library/Model/Location.cs
@@ -4,8 +4,23 @@ namespace Battleship.BLL.Models
 {
     public class Location
     {
-        //row
+        public int Row { get; }
+        public int Column { get; }
+        
+        public Location(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
 
-        //colum
+        public override bool Equals(object obj) =>
+            return obj is Location && this == (Location)obj;
+
+        public override int GetHashCode() => (Row, Column).GetHashCode();
+
+        public static bool operator ==(Location x, Location y) =>
+            x.Row == y.Row && x.Column == y.Column;
+
+        public static bool operator !=(Location x, Location y) => return !(x == y);
     }
 }
