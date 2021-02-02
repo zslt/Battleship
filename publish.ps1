7cd444dbeb6fd41a350a965cc87c8b08c5cb8069
Zsolt Keczer <keczerzsolt@gmail.com>
2021-02-02 18:20:17 +0100

    publish.ps1 clean folder before publish

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
+Remove-Item .\artifacts -Recurse -ErrorAction Ignore
 New-Item -ItemType Directory -Force -Path .\artifacts
 dotnet publish .\src\Battleship.Cli\Battleship.Cli.csproj -c Release -r win-x64
